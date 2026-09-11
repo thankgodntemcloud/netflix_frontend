3536c1402a7795510295ceed7d1003c55e890b99
ThankGod Ntem <teegeentem@gmail.com>
2026-09-11 09:40:47 +0100

    Update axios base URL to new endpoint

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://18.221.67.127:8080',
+    baseURL:'http://34.238.28.137:8080',
     headers: {
         'Content-Type': 'application/json',
     },
